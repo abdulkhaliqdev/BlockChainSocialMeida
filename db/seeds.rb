ba0855fcf519c44ea2bbca788902b3cd3212132b
abdulkhaliqdev <abdulkhaliqdev@gmail.com>
2022-12-18 19:23:16 +0500

    Add login and Signup functionality

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+if Doorkeeper::Application.count.zero?
+  Doorkeeper::Application.create(name: "web client", redirect_uri: "", scopes: "")
+end
